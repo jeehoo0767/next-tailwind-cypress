b15f014eaa2cc7ca38b7a42ac8b1833940f97a72
jeehoo <jeehoo0767@naver.com>
2022-10-16 21:59:50 +0900

    todo 완료 기능 추가

--- components/TodoRow.tsx
+++ components/TodoRow.tsx
@@ -1,19 +1,20 @@
 interface TodoRow {
   text: string
   isComplete: boolean
+  handleComplete: (targetIndex: number) => void
 }
 const TodoRow = (props: TodoRow) => {
   return (
     <>
       {props.isComplete ? (
-        <li className={'flex items-center mb-s8 line-through opacity-50'}>
+        <li className={'flex items-center mb-s8 line-through opacity-50 cursor-pointer'} onClick={props.handleComplete}>
           <span
             className={'rounded-[50%] w-[10px] h-[10px] inline-block bg-primary-default mr-s8 border-[1px] border-primary-color'}
           ></span>
           {props.text}
         </li>
       ) : (
-        <li className={'flex items-center mb-s8'}>
+        <li className={'flex items-center mb-s8 cursor-pointer'} onClick={props.handleComplete}>
           <span className={'rounded-[50%] w-[8px] h-[8px] inline-block mr-s8 border-[1px] border-primary-color'}></span>
           {props.text}
         </li>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import TodoRow from '../components/TodoRow'
 interface TodoList {
   text: string
   isComplete: boolean
 }
 const Home: NextPage = () => {
+  const inputRef = useRef<any>(null)
   const [todoList, setTodoList] = useState<TodoList[]>([])
   const [isEdit, setIsEdit] = useState(false)
   const [todoValue, setTodoValue] = useState('')
@@ -17,6 +18,24 @@ const Home: NextPage = () => {
     setIsEdit(false)
     setTodoValue('')
   }
+
+  const handleComplete = (targetIndex: number) => {
+    const nextList = todoList.map((item, index) => {
+      if (targetIndex === index) {
+        return {
+          text: item.text,
+          isComplete: !item.isComplete,
+        }
+      } else {
+        return {
+          ...item,
+        }
+      }
+    })
+    console.log(targetIndex)
+    setTodoList(nextList)
+  }
+
   return (
     <>
       <main id={'todo-container'} className={'text-[20px] p-s32 bg-primary-background h-[100vh]'}>
@@ -30,9 +49,9 @@ const Home: NextPage = () => {
 
         <section className={'bg-white mt-s12 p-s16 max-w-[576px] mx-auto text-primary-color shadow-default relative'}>
           <ul>
-            {todoList.map((item) => (
+            {todoList.map((item, index) => (
               <>
-                <TodoRow text={item.text} isComplete={item.isComplete} />
+                <TodoRow text={item.text} isComplete={item.isComplete} handleComplete={() => handleComplete(index)} />
               </>
             ))}
           </ul>
@@ -40,10 +59,16 @@ const Home: NextPage = () => {
             <div className={'flex items-center mb-s8 animate-appear-todo translate-x-[80px]'}>
               <span className={'rounded-[50%] w-[8px] h-[8px] inline-block mr-s8 border-[1px] border-primary-color'}></span>
               <input
+                ref={inputRef}
                 type="text"
                 className={'border-2 rounded-[4px] py-[4px] px-[8px]'}
                 value={todoValue}
                 onChange={(e) => setTodoValue(e.target.value)}
+                onKeyUp={(e) => {
+                  if (e.key === 'Enter') {
+                    submitTodo(todoValue)
+                  }
+                }}
               />
               <button className={'ml-s8'} onClick={() => submitTodo(todoValue)}>
                 Submit
@@ -55,7 +80,12 @@ const Home: NextPage = () => {
             className={
               'bg-primary-default w-[180px] py-s8 text-center text-white rounded-[30px] absolute left-[50%] translate-x-[-50%] shadow-default hover:scale-105'
             }
-            onClick={() => setIsEdit(true)}
+            onClick={() => {
+              setIsEdit(true)
+              setTimeout(() => {
+                inputRef?.current?.focus()
+              }, 100)
+            }}
           >
             + New Task
           </button>
