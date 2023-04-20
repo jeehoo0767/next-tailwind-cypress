c5c5984ecde026c757d240f127d664d5ad02efec
jeehoo0767 <jeehoo0767@naver.com>
2023-04-21 02:32:40 +0900

    Fix Type error

--- components/TodoRow.tsx
+++ components/TodoRow.tsx
@@ -15,7 +15,7 @@ const TodoRow = (props: TodoRow) => {
         <li
           data-cy="todo-row-complete"
           className={'flex items-center mb-s8 line-through opacity-50 cursor-pointer'}
-          onClick={props.handleComplete}
+          onClick={() => props.handleComplete}
         >
           <span
             className={'rounded-[50%] w-[10px] h-[10px] inline-block bg-primary-default mr-s8 border-[1px] border-primary-color'}
@@ -26,7 +26,7 @@ const TodoRow = (props: TodoRow) => {
           </span>
         </li>
       ) : (
-        <li data-cy="todo-row-no-complete" className={'flex items-center mb-s8 cursor-pointer'} onClick={props.handleComplete}>
+        <li data-cy="todo-row-no-complete" className={'flex items-center mb-s8 cursor-pointer'} onClick={() => props.handleComplete}>
           <span className={'rounded-[50%] w-[8px] h-[8px] inline-block mr-s8 border-[1px] border-primary-color'}></span>
           {props.text}
         </li>
